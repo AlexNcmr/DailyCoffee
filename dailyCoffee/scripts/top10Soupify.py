89138ff50daf9967310d6ac07ae92709d590e215
Alex Newcomer <alexjnewcomer@gmail.com>
2017-01-29 11:16:21 -0600

    updated the web scraper to handle the ASK HN: type of links which were previously skipped!

--- dailyCoffee/scripts/top10Soupify.py
+++ dailyCoffee/scripts/top10Soupify.py
@@ -11,6 +11,9 @@ def HNTop10():
 
     HN = ur.urlopen("https://news.ycombinator.com/").read()
     soup = BeautifulSoup(HN)
+    linkMatch = re.compile(r'href="(\S*)"')
+    links = []
+    linkTags = []
 
     #grab the first 10 link tags
     #the link tags have the class "storylink", limit to 10 matches
@@ -18,10 +21,17 @@ def HNTop10():
 
     #convert thelink tags to strings so we can use regex to extract the links
     linkTags = list(map(str, linkTags))
-    linkMatch = re.compile(r'(http\S*)"')
 
-    #use the compiled regex to find all matches (the first and only one will be what we need)
+    #use regex to find all matches (the first and only one will be what we need)
     #findall() returns a list of matches, so use the first one by default
-    links = [linkMatch.findall(tag)[0] for tag in linkTags]
+    for tag in linkTags:
+        #grab all regex matches
+        match = linkMatch.findall(tag)
+        link = match[0]
+        #if the match starts with http, it is a link to an external site and can be passed directly.
+        #else it is to a comment section of HN, like the "Ask HN" posts
+        if not link[:4] =="http":
+            link = "https://news.ycombinator.com/" + link
+        links.append(link)
 
     return links
